1b752f6db70a1f79575ac0b5d0a271659f78894d
EC2 Default User <ec2-user@ip-172-31-12-175.eu-west-1.compute.internal>
2025-04-02 00:52:21 +0000

    Initial commit 2

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ec2-user/environment/ArtAuthenticator/venv"
+set -gx VIRTUAL_ENV "/home/ec2-user/environment/Art_Authentication/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
